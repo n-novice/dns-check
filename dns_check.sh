db722bbec07097307a50ea6c988e8140566cabb6
のーびす <46832832+n-novice@users.noreply.github.com>
2020-12-13 15:51:01 +0900

    Update dns_check.sh

--- dns_check.sh
+++ dns_check.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
-URL="./domain_list.txt"
-DNS="./dns_list.txt"
-Op=" -t "
-FNAME=results_`date +%Y%m%d%H%M%S`.txt
+URL="./domain_list.txt"  #チェックリスト
+DNS="./dns_list.txt"     #DNSサーバリスト
+Op=" -t "                #dig option
+FNAME=results_`date +%Y%m%d%H%M%S`.txt #出力するリザルトファイル名
 
+#チェックリストの読み込み
 cat "$URL" | while read line
 do
-  query=`echo ${line} | cut -d ',' -f 1`
-  domain=`echo ${line} | cut -d ',' -f 2`
-  query2=$Op$query
-  echo "//=========================================================================================" >> $FNAME
-  echo "//Domain: ${domain}(Quuery: ${query})  [dig +noall +answer ${query2} <DNS> ${domain}]" >> $FNAME
-  echo "//-----------------------------------------------------------------------------------------" >> $FNAME
+  #カラム数が２であることを確認（クエリ.domain)
+  col=`echo ${line}  | awk -F "," '{print NF}'`
+  if [ $((col)) -eq 2 ]; then
 
-  cat "$DNS" | while read line2
-  do
-    com=$(dig +noall +answer $query2 @${line2} "$domain")
-    ans=${com}
-    echo "---  "  >> $FNAME
-    echo -e ">>> DNS : $line2" >> $FNAME
-    echo -e "$ans" >> $FNAME
-    echo ""  >> $FNAME
-    usleep 300000
+    #query,domainに代入
+    query=`echo ${line} | cut -d ',' -f 1`
+    query2=$Op$query
+    domain=`echo ${line} | cut -d ',' -f 2`
+    echo "//=========================================================================================" >> $FNAME
+    echo "//Domain: ${domain}(Quuery: ${query})  [dig +noall +answer ${query2} <DNS> ${domain}]" >> $FNAME
+    echo "//-----------------------------------------------------------------------------------------" >> $FNAME
 
-  done < "$DNS"
+    #DNSサーバリストからサーバ情報を１行読み込む
+    cat "$DNS" | while read line2
+    do
+      #コマンド実行
+      com=$(dig +noall +answer $query2 @${line2} "$domain")
+      ans=${com}
+      echo "---  "  >> $FNAME
+      echo -e ">>> DNS : $line2" >> $FNAME
+      echo -e "$ans" >> $FNAME
+      echo ""  >> $FNAME
 
+      #スリープ時間
+      usleep 300000
+    done < "$DNS"
+  else
+    echo "---"  >> $FNAME
+    echo "Syntax error : ${line}" >> $FNAME
+    echo "---"  >> $FNAME
+  fi
 done < "$URL"
 
+#出力リザルトの表示
 cat $FNAME
